18bccbba9de023d54968b59ab26c5638f1f52145
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-02-11 18:21:07 +0100

    can turn now

--- public/client.js
+++ public/client.js
@@ -1,3 +1,3 @@
 document.querySelector(".turn-button").addEventListener("click", () => {
-    document.querySelector(".card-container").classList.toggle("turn")
+    document.querySelector(".card-inner").classList.toggle("flipped");
 })
